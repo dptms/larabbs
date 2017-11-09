8e721e6d25604b685688122f9f893e67023bbf72
dp~ <457509824@qq.com>
2017-11-09 22:48:17 +0800

    topics首页改版

---
+++ resources/views/topics/_sidebar.blade.php
@@ -0,0 +1,3 @@
+<div class="panel">
+    <div class="panel-body">右侧边栏导航</div>
+</div>
---
+++ resources/views/topics/_topic_list.blade.php
@@ -0,0 +1,48 @@
+@if(count($topics))
+    <ul class="media-list">
+        @foreach($topics as $topic)
+            <li class="media">
+                <div class="media-left">
+                    <a href="{{route('users.show',$topic->user)}}">
+                        <img src="{{$topic->user->avatar}}" alt="" class="media-object img-thumbnail img-responsive"
+                             width="52" height="52">
+                    </a>
+                </div>
+
+                <div class="media-body">
+                    <div class="media-heading">
+                        <a href="{{route('topics.show',$topic)}}">
+                            {{$topic->title}}
+                        </a>
+                        <a href="#" class="pull-right">
+                            <span class="badge" aria-hidden="true"> {{$topic->reply_count}} </span>
+                        </a>
+                    </div>
+
+                    <div class="media-body meta">
+                        <a href="#" title="{{$topic->category->name}}">
+                            <span class="glyphicon glyphicon-folder-open" aria-hidden="true"></span>
+                            {{$topic->category->name}}
+                        </a>
+                        <span> · </span>
+                        <a href="{{route('users.show',$topic->user)}}" title="{{$topic->user->name}}">
+                            <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+                            {{$topic->user->name}}
+                        </a>
+                        <span> · </span>
+                        <a href="#">
+                            <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+                            {{$topic->created_at->diffForHumans()}}
+                        </a>
+                    </div>
+                </div>
+
+                @if(!$loop->last)
+                    <hr>
+                @endif
+            </li>
+        @endforeach
+    </ul>
+@else
+    <div class="empty-block">暂无数据</div>
+@endif
