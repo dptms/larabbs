1d2ea48a8c833b88c576e917aec8c397f4d17ec4
dp~ <457509824@qq.com>
2017-11-19 23:06:46 +0800

    使用队列 对话题slug字段进行处理

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -2,7 +2,7 @@
 
 namespace App\Observers;
 
-use App\Handlers\SlugTranslateHandler;
+use App\Jobs\TranslateSlug;
 use App\Models\Topic;
 
 // creating, created, updating, updated, saving,
@@ -14,10 +14,13 @@ class TopicObserver
     {
         $topic->body = clean($topic->body,'user_topic_body');
         $topic->excerpt = make_excerpt($topic->body);
+    }
 
+    public function saved(Topic $topic)
+    {
         // 如果 topic slug字段没有值，使用翻译对 title 进行翻译
         if(!$topic->slug){
-            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+            dispatch(new TranslateSlug($topic));
         }
     }
 }
