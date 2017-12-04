2688876cd45e4e88e08db11f0c6117fca82b511a
dp~ <457509824@qq.com>
2017-12-04 23:42:03 +0800

    加入用户切换工具 sudo-su

--- app/Policies/Policy.php
+++ app/Policies/Policy.php
@@ -2,21 +2,18 @@
 
 namespace App\Policies;
 
+use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
 class Policy
 {
     use HandlesAuthorization;
 
-    public function __construct()
-    {
-        //
-    }
-
-    public function before($user, $ability)
+    public function before(User $user, $ability)
 	{
-	    // if ($user->isSuperAdmin()) {
-	    // 		return true;
-	    // }
+	    // 如果用户拥有管理内容的权限的话，即授权通过
+        if($user->can('manage_contents')){
+            return true;
+        }
 	}
 }
