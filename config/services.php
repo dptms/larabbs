f71e024a4176e07554d92b9ce2f0bf81c739dccd
dp~ <457509824@qq.com>
2018-03-30 18:19:54 +0800

    add socialiteproviders

--- config/services.php
+++ config/services.php
@@ -20,7 +20,7 @@ return [
     ],
 
     'ses' => [
-        'key'    => env('SES_KEY'),
+        'key' => env('SES_KEY'),
         'secret' => env('SES_SECRET'),
         'region' => 'us-east-1',
     ],
@@ -30,13 +30,18 @@ return [
     ],
 
     'stripe' => [
-        'model'  => App\Models\User::class,
-        'key'    => env('STRIPE_KEY'),
+        'model' => App\Models\User::class,
+        'key' => env('STRIPE_KEY'),
         'secret' => env('STRIPE_SECRET'),
     ],
 
     'baidu_translate' => [
         'app_id' => env('BAIDU_TRANSLATE_APPID'),
-        'key'    => env('BAIDU_TRANSLATE_KEY'),
+        'key' => env('BAIDU_TRANSLATE_KEY'),
+    ],
+    'weixin' => [
+        'client_id' => env('WEIXIN_KEY'),
+        'client_secret' => env('WEIXIN_SECRET'),
+        'redirect' => env('WEIXIN_REDIRECT_URI'),
     ],
 ];
