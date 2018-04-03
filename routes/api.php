995e78177d9a16a610ffd2fd46556d261e71772e
dp~ <457509824@qq.com>
2018-04-03 11:05:43 +0800

    replies index

--- routes/api.php
+++ routes/api.php
@@ -51,6 +51,8 @@ $api->version('v1', [
         $api->get('topics', 'TopicsController@index')->name('api.topics.index');
         $api->get('topics/{topic}', 'TopicsController@show')->name('api.topics.show');
         $api->get('users/{user}/topics', 'TopicsController@userIndex')->name('api.users.topics.index');
+        $api->get('topics/{topic}/replies', 'RepliesController@index')->name('api.topics.replies.index');
+        $api->get('users/{user}/replies', 'RepliesController@userIndex')->name('api.users.replies.index');
         // 需要 token 验证的接口
         $api->group(['middleware' => 'api.auth'], function ($api) {
             // 当前登录用户信息
