0a645fd791b887218c9eac50d3c9390f6d7f4f79
dp~ <457509824@qq.com>
2018-04-03 10:51:20 +0800

    reply delete

--- app/Http/Controllers/Api/RepliesController.php
+++ app/Http/Controllers/Api/RepliesController.php
@@ -6,17 +6,28 @@ use App\Http\Requests\Api\ReplyRequest;
 use App\Models\Reply;
 use App\Models\Topic;
 use App\Transformers\ReplyTransformer;
-use Illuminate\Http\Request;
 
 class RepliesController extends Controller
 {
     public function store(ReplyRequest $request, Topic $topic, Reply $reply)
     {
-        $reply->content = $request->content;
+        $reply->content = $request->get('content');
         $reply->topic_id = $topic->id;
         $reply->user_id = $this->user()->id;
         $reply->save();
 
         return $this->response->item($reply, new ReplyTransformer())->setStatusCode(201);
+    }
+
+    public function destroy(Topic $topic, Reply $reply)
+    {
+        if ($reply->topic_id != $topic->id) {
+            return $this->response->errorBadRequest();
+        }
+
+        $this->authorize('destroy', $reply);
+        $reply->delete();
+
+        return $this->response->noContent();
     }
 }
