5b0136f4af30e8ed55e903f3b974bc9acf00a6da
dp~ <457509824@qq.com>
2018-08-24 14:43:45 +0800

    torann/geoip

--- config/app.php
+++ config/app.php
@@ -179,6 +179,7 @@ return [
 
         \App\Providers\EasySmsServiceProvider::class,
         \App\Providers\JpushServiceProvider::class,
+        App\Providers\ComposerServiceProvider::class,
     ],
 
     /*
--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -3,7 +3,7 @@
         <p class="pull-left">
             由 <a href="https://github.com/dptms" target="_blank">Dp</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
         </p>
-
         <p class="pull-right"><a href="mailto:{{setting('contact_email')}}">联系我们</a></p>
+        <p class="pull-right" style="margin-right: 20px"> 来自: {{$location}}</p>
     </div>
 </footer>
